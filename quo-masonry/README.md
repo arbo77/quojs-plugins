30b86add0aa067708ff954d5b3ba1f82b289bfb0
Ari Wibowo <arbo77@gmail.com>
2013-04-12 16:06:15 +0700

    first commit

---
+++ quo-masonry/README.md
@@ -0,0 +1,8 @@
+QuoJS-Plugins
+=============
+
+
+Experimental of QuoJS-plugins.
+
+First commit of this Git, contain quo-masonry plugin with no options and force
+4 columns. It's should be using an options next commit :) 
---
+++ quo-masonry/quo.masonry.js
@@ -0,0 +1,20 @@
+(function($$){
+	$$.fn.masonry = function(options){
+		var i=0;
+		var cols  = 4;
+		this.append("<div class='tmp'></div>");
+		this.children().each(function(){
+			if(this.className !== "tmp"){
+				$$(".tmp").append(this)
+			}
+		});
+		this.addClass("mason");
+		for(c=0;c<cols;c++){
+			this.append("<div class='col'></div>");
+		}
+		$$(".tmp").children().each(function(){
+		  $$($$(".mason .col")[i%4]).append(this)
+		  i++;
+		});
+	}
+})(Quo);
