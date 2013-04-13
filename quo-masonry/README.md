cfba90910c9eed6bc8f331b6d512c26269251bbf
Ari Wibowo <arbo77@gmail.com>
2013-04-13 17:42:52 +0700

    See quo-masonry readme

--- quo-masonry/README.md
+++ quo-masonry/README.md
@@ -1,8 +1,20 @@
 QuoJS-Plugins
 =============
 
-
 Experimental of QuoJS-plugins.
 
 First commit of this Git, contain quo-masonry plugin with no options and force
 4 columns. It's should be using an options next commit :) 
+
+Changelog
+=========
+2013/04/12 
+- First commit
+
+2013/04/13 
+- Change entire masonry class
+- adding init method into Masonry class
+- adding add_item method into Masonry class
+- adding draw_item method into Masonry class
+- adding masonry.append method into quo-masonry plugin
+- adding masonry.prepend method into quo-masonry plugin
--- quo-masonry/quo.masonry.js
+++ quo-masonry/quo.masonry.js
@@ -1,22 +1,60 @@
 (function($$){
-	$$.fn.masonry = function(options){
-		var i=0;
-		var cols  = 4;
-		this.append("<div class='tmp'></div>");
-		this.children().each(function(){
-			if(this.className !== "tmp"){
-				$$(".tmp").append(this)
-			}
-		});
-		this.addClass("mason");
-		for(c=0;c<cols;c++){
-			this.append("<div class='col'></div>");
-		}
-		$$(".tmp").children().each(function(){
-		  $$($$(".mason .col")[i%4]).append(this)
-		  i++;
-		});
+	var Masonry = {
+		element: null,
+		childs: [],
+		data: {
+			col_index: 0,
+			col_number: 4,
+		},
 		
-		$$(".tmp").remove();
-	}
+		init: function(element, options){
+			/**
+			 * init masonry element, and grab all 1st level child in element
+			 */
+			this.element = element;
+			this.element.children().each(function(){
+				Masonry.childs.push(this)
+			});
+			this.element.addClass("mason");
+			/**
+			 * create n-columns
+			 * n-columns should be fluid next time
+			 */
+			for(c=0;c<this.data.col_number;c++){
+				this.element.append("<div class='col'></div>");
+			}
+			this.draw_item();
+		},
+		
+		draw_item: function(){
+			/**
+			 * clear all n-column's item, and
+			 * moving 1st level child into appropriate column,
+			 * and marking next column index
+			 */
+			$$(".mason .col").empty();
+			for(i=0;i<this.childs.length;i++){
+				this.add_item(this.childs[i],i)
+			}
+		},
+		
+		add_item: function(item, index){
+			this.data.col_index = (index%this.data.col_number)
+			$$($$(".mason .col")[this.data.col_index]).append(item)
+		},
+	};
+	
+	$$.fn.masonry = function(options){
+		Masonry.init(this,options)
+	};
+	
+	$$.fn.masonry.append = function(item){
+		Masonry.childs.push(item);
+		Masonry.add_item(item,Masonry.childs.length-1)
+	};
+	
+	$$.fn.masonry.prepend = function(item){
+		Masonry.childs.unshift(item[0]);
+		Masonry.draw_item();
+	};
 })(Quo);
